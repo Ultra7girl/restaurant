251bf2f6c75385f225aecad4bb8eed3ec5f15322
Matha Lokanphai <mathal@tencent.co.th>
2018-01-08 18:43:45 +0700

    initial config

---
+++ src/client.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { BrowserRouter } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+
+// import store from './modules/Blog/redux/store'
+// import App from './app'
+
+// const container = document.getElementById('root')
+
+// ReactDOM.hydrate(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   container
+// )
+console.log('secureuser');
---
+++ src/server.js
@@ -0,0 +1,58 @@
+import 'isomorphic-fetch'
+
+import express from 'express'
+// import React from 'react'
+// import ReactDOM from 'react-dom/server'
+// import { StaticRouter } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+
+// import store from './modules/Blog/redux/store'
+// import App from './app'
+
+const port = 8080
+const app = express()
+
+app.listen(port)
+app.use(express.static('public'))
+
+app.get('*', function(req, res) {
+  // const context = {}
+
+  // const content = ReactDOM.renderToString(
+  //   <Provider store={store}>
+  //     <StaticRouter location={req.url} context={context}>
+  //       <App />
+  //     </StaticRouter>
+  //   </Provider>
+  // )
+
+  const html = `
+    <html>
+      <head>
+        <title>JavaScript Tutorial</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <style>
+          * {
+            box-sizing: border-box;
+            margin: 0px;
+            padding: 0px;
+          }
+          html {
+            font-size: 87.5%;
+          }
+          body {
+            font-family: arial, 'sans-serif';
+            font-size: 1em;
+          }
+        </style>
+      </head>
+      <body>
+        <div id="root">1234</div>
+        <script src="/build/client.bundle.js"></script>
+      </body>
+    </html>
+  `
+
+  res.send(html)
+})
