76e494f99b17ead97c81c79c4d6a5a70c1273b67
Matha Lokanphai <mathal@tencent.co.th>
2018-01-13 15:14:18 +0700

    can show rating

--- pages/item.js
+++ pages/item.js
@@ -8,9 +8,10 @@ import gql from 'graphql-tag'
 import page from '../hocs/page'
 import { Link } from '../routes'
 
+
+
 function itemPage({ data }) {
 	const { loading, postMenu } = data
-	console.log(postMenu)
 
 	if (loading === true) return 'Loading...'
 	return (
@@ -39,12 +40,69 @@ function itemPage({ data }) {
 				.clearFix {
 					clear: both;
 				}
+
+
+				.rating {
+						float:left;
+				}
+
+				/* :not(:checked) is a filter, so that browsers that don’t support :checked don’t 
+					follow these rules. Every browser that supports :checked also supports :not(), so
+					it doesn’t make the test unnecessarily selective */
+				.rating:not(:checked) > input {
+						position:absolute;
+						top:-9999px;
+						clip:rect(0,0,0,0);
+				}
+
+				.rating:not(:checked) > label {
+						float:right;
+						width:1em;
+						padding:0 .1em;
+						overflow:hidden;
+						white-space:nowrap;
+						cursor:pointer;
+						font-size:200%;
+						line-height:1.2;
+						color:#ddd;
+						text-shadow:1px 1px #bbb, 2px 2px #666, .1em .1em .2em rgba(0,0,0,.5);
+				}
+
+				.rating:not(:checked) > label:before {
+						content: '★ ';
+				}
+
+				.rating > input:checked ~ label {
+						color: #f70;
+						text-shadow:1px 1px #c60, 2px 2px #940, .1em .1em .2em rgba(0,0,0,.5);
+				}
+
+				.rating:not(:checked) > label:hover,
+				.rating:not(:checked) > label:hover ~ label {
+						color: gold;
+						text-shadow:1px 1px goldenrod, 2px 2px #B57340, .1em .1em .2em rgba(0,0,0,.5);
+				}
+
+				.rating > input:checked + label:hover,
+				.rating > input:checked + label:hover ~ label,
+				.rating > input:checked ~ label:hover,
+				.rating > input:checked ~ label:hover ~ label,
+				.rating > label:hover ~ input:checked ~ label {
+						color: #ea0;
+						text-shadow:1px 1px goldenrod, 2px 2px #B57340, .1em .1em .2em rgba(0,0,0,.5);
+				}
+
+				.rating > label:active {
+						position:relative;
+						top:2px;
+						left:2px;
+				}
 			`}</style>
 			<Head>
 				<title>Restaurant </title>
 			</Head>
 			<div className="container">
-				{postMenu.map(function(item) {
+				{postMenu.map(function (item) {
 					return (
 						<div key={item.id}>
 							<div className="main-left">
@@ -60,6 +118,14 @@ function itemPage({ data }) {
 									<p>ราคา {item.price}.-</p>
 									<p>
 										{item.rating.avgRating}({item.rating.totalRating} Ratings)
+										<div className="rating">
+											<input type="radio" id="star5" checked={(item.rating.avgRating <= 5)} name="rating" value="5" /><label htmlFor="star5" title="Rocks!">5 stars</label>
+											<input type="radio" id="star4" checked={(item.rating.avgRating <= 4)} name="rating" value="4" /><label htmlFor="star4" title="Pretty good">4 stars</label>
+											<input type="radio" id="star3" checked={(item.rating.avgRating <= 3)} name="rating" value="3" /><label htmlFor="star3" title="Meh">3 stars</label>
+											<input type="radio" id="star2" checked={(item.rating.avgRating <= 2)} name="rating" value="2" /><label htmlFor="star2" title="Kinda bad">2 stars</label>
+											<input type="radio" id="star1" checked={(item.rating.avgRating <= 1)} name="rating" value="1" /><label htmlFor="star1" title="Sucks big time">1 star</label>
+										</div>
+
 									</p>
 									<input type="button" value="Order" />
 								</div>
@@ -68,7 +134,7 @@ function itemPage({ data }) {
 								<p>
 									<strong>Reviews</strong>
 								</p>
-								{item.relateComments.map(function(comments) {
+								{item.relateComments.map(function (comments) {
 									{
 										return <p key={comments.id}>{comments.body}</p>
 									}
