4d5211143b63f60651c46ec28bb3b25d0cbe75f8
Rattikan Apichai <nik1803@gmail.com>
2018-01-18 09:09:33 +0700

    revised orderUpdater

--- store/orderUpdater.js
+++ store/orderUpdater.js
@@ -4,35 +4,31 @@ export default function orderUpdater(state = emtpyOrder, action) {
 
   if (type === 'CHECK_BILL') {
     return emtpyOrder
-  }
+  } else if (type === 'ADD_ORDER') {
+    const completedItem = state.filter(function(basket) {
+      return basket.id === item.id
+    }).length
 
-  const completedItem = state.filter(function(basket) {
-    return basket.id === item.id
-  }).length
-
-  switch (type) {
-    case 'ADD_ORDER':
-      if (completedItem === 0) {
-        return state.concat({
-          id: item.id,
-          name: item.name,
-          price: item.price,
-          amount: 1
-        })
-      } else {
-        return state.map(function(basket) {
-          if (basket.id === item.id) {
-            return {
-              ...basket,
-              amount: basket.amount + 1
-            }
+    if (completedItem === 0) {
+      return state.concat({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        amount: 1
+      })
+    } else {
+      return state.map(function(basket) {
+        if (basket.id === item.id) {
+          return {
+            ...basket,
+            amount: basket.amount + 1
           }
+        }
 
-          return basket
-        })
-      }
-
-    default:
-      return state
+        return basket
+      })
+    }
+  } else {
+    return state
   }
 }
